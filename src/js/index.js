188435e1af07105e30867d1c9baa03bafb3bc8ca
Karol Czyżewski <Karolek0493@wp.pl>
2020-11-07 16:31:58 +0100

    add: listener to close on first element

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import '../scss/app.scss';
+
+const paragraph = document.querySelector('.item__paragraph--js')
+const button = document.querySelector('.item__button');
+
+
+const closeItem = () => {
+    paragraph.classList.toggle('closed')
+}
+
+button.addEventListener('click', closeItem);
