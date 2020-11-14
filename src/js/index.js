6c9f5eebaa4600819abb653bfb1e69a2edace0f3
Karol Czyżewski <Karolek0493@wp.pl>
2020-11-14 05:59:42 +0100

    add: open/close between faq items

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,15 @@ import '../scss/app.scss';
 
 const allItems = document.querySelectorAll('.item')
 
+const removeClass = () => {
+    allItems.forEach(item => {
+        item.classList.remove('open');
+    })
+};
+
 allItems.forEach(item => {
     item.addEventListener('click', () => {
+        removeClass();
         item.classList.toggle('open');
     })
 })
